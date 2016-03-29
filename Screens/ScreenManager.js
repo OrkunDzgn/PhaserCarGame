88f60caff44d3ce8869f8941710e268585640e2c
OrkunDuzgun <orkundzgn@gmail.com>
2016-03-29 23:30:23 +0300

    Car Added with separate wheel physics

--- Screens/ScreenManager.js
+++ Screens/ScreenManager.js
@@ -13,61 +13,61 @@ function preload() {
     // load the sprite texture
     game.load.image('road1', 'images/map1.png');
     game.load.image('road2', 'images/map2.png');
-    game.load.image('carBody', 'images/carparts/body.png');
+    game.load.image('carbody', 'images/body.png');
+    game.load.image('wheel', 'images/smallWheel.png');
 
     // load the physics data json
     game.load.physics('physicsData', 'road.json');
     game.load.physics('physicsData2', 'road2.json');
-    game.load.physics('carBodyPhysics', 'jeep_body.json')
+    game.load.physics('objectPhysics', 'jeepCol.json');
+    game.load.physics('wheelPhysics', 'wheelCol.json');
 }
 
 var polygonCollisionSprite;
 var objColSprite;
+var wheelSprite;
 var cursors;
 
 function create() {
 
     maxSpeed = 850;
-
-    //game.add.tileSprite(0, 0, 3200, 600, 'road2');
-
     game.world.setBounds(0, 0, 3800, 600);
-
-    // start the P2JS physics system
     game.physics.startSystem(Phaser.Physics.P2JS);
-
-    //set some initial gravity
     game.physics.p2.gravity.y = 500;
-
     game.physics.p2.friction = 2;
 
-    // add our polygon collider sprite
-    // and give it physics
     polygonCollisionSprite = game.add.sprite(1600, 320, 'road2');
-    objColSprite = game.add.sprite(0, 0, 'carBody');
-    
+    objColSprite = game.add.sprite(0, 0, 'carbody');
+    wheelSprite = game.add.sprite(0, 0, 'wheel');
 
-    game.physics.p2.enable([polygonCollisionSprite, objColSprite], false);
-    //game.physics.p2.enable(objColSprite, true);
+    game.physics.p2.enable([polygonCollisionSprite, objColSprite, wheelSprite], false);
 
-    // remove all of the current collision shapes from the physics body
+    //var constraint = game.physics.p2.createLockConstraint(objColSprite, wheelSprite, [45, -52], 0);
+
     polygonCollisionSprite.body.clearShapes();
     objColSprite.body.clearShapes();
 
 
-    // load our polygon physics data
     polygonCollisionSprite.body.loadPolygon('physicsData2', 'map2');
-    objColSprite.body.loadPolygon('carBodyPhysics', 'body');
+    objColSprite.body.loadPolygon('objectPhysics', 'body');
+    wheelSprite.body.loadPolygon('wheelPhysics', 'smallWheel');
 
-    objColSprite.body.data.gravityScale = 1,3;
+    objColSprite.body.data.gravityScale = 1, 3;
     polygonCollisionSprite.body.static = true;
+    
 
     objColSprite.mass = 100;
+    wheelSprite.mass = 50;
     objColSprite.allowRotation = true;
+    wheelSprite.allowRotation = true;
+
+
+    wheelSprite.anchor.setTo(0.5, 0.5);
 
     cursors = game.input.keyboard.createCursorKeys();
 
     game.camera.follow(objColSprite);
+
 
 }
 function update() {
@@ -75,8 +75,9 @@ function update() {
         objColSprite.body.y -= 20;
     }
     if (cursors.right.isDown) {
-        if (objColSprite.body.velocity.x < maxSpeed)
-        objColSprite.body.velocity.x += 7;
+        wheelSprite.body.angle += 10;
+        //if (objColSprite.body.velocity.x < maxSpeed)
+          //  objColSprite.body.velocity.x += 7;
     } else if (cursors.left.isDown) {
         objColSprite.body.velocity.x -= 7;
     }
